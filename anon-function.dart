d69cfaa42aca24b843a50817f3d7b208274ad6cc
Jão Carlos Castro <81690318+Jao-br@users.noreply.github.com>
2022-09-21 09:58:29 -0300

    Update anon-function.dart

--- anon-function.dart
+++ anon-function.dart
@@ -5,10 +5,12 @@ void main() {
 void timer() {
   int i = 0;
 
+  print('Computing data...');
   while (i <= 10) {
     print(i);
     i++;
   }
+  print('FINISHED! \n');
 }
 
 void saudacoes(
@@ -17,6 +19,8 @@ void saudacoes(
   String? cliente,
   required Function body,
 }) {
+  body();
+
   print('Saudações do ${nome.toUpperCase()}!');
 
   String c = cliente ?? 'Cliente não Inforado';
