f94b7fbae2a74f3147a74de8c9336d963a30343b
Jão Carlos Castro <81690318+Jao-br@users.noreply.github.com>
2022-09-21 09:50:25 -0300

    Update anon-function.dart

--- anon-function.dart
+++ anon-function.dart
@@ -1,27 +1,34 @@
 void main() {
-  saudacoes('Jão', cliente: 'Luca');
-  parameters('a', null, d: 'Luis', e: 'teste');
+  saudacoes('Jão', cliente: 'Luca', body: timer);
+}
+
+void timer() {
+  int i = 0;
+
+  while (i <= 10) {
+    print(i);
+    i++;
+  }
 }
 
 void saudacoes(
   String nome, {
   bool mostrarAgora = true,
   String? cliente,
+  required Function body,
 }) {
   print('Saudações do ${nome.toUpperCase()}!');
-  
+
   String c = cliente ?? 'Cliente não Inforado';
   print('seja bem vinde ${c.toUpperCase()}!');
-  
-  if(mostrarAgora){
+
+  if (mostrarAgora) {
     print('Time of purchase: ${agora()}');
-  }  
+  }
 }
 
-String agora(){
+String agora() {
   DateTime agora = DateTime.now();
-    return agora.toString();
-    
+  return agora.toString();
 }
- 
 
