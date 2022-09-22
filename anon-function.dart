988591ba5d62e2a35684fe5c5a251ba936bf9267
Jão Carlos Castro <81690318+Jao-br@users.noreply.github.com>
2022-09-22 09:47:30 -0300

    Update anon-function.dart

--- anon-function.dart
+++ anon-function.dart
@@ -1,33 +1,26 @@
 void main() {
-  saudacoes('Jão', cliente: 'Luca', body: negativeTimer);
-}
-
-void timer(int i) {
+  saudacoes('Jão', cliente: 'Luca', body: (i, j) {
   print('Processing data...');
-  for (int j = 0; j <= i; j++) {
+  if(i >= j){ 
+    j++;
+    print('$j%');  
+  }
+  else if(i <= j){
+    j--;
     print('$j%');
   }
   print('FINISHED! \n');
-}
-
-void negativeTimer(int i) {
-  print('Processing data...');
-  for (int k = 100; k >= i; k--) {
-    print('$k%');
-  }
-  print('FINISHED! \n');
+},
+);
 }
 
 void saudacoes(
   String nome, {
   bool mostrarAgora = true,
   String? cliente,
-  required Function(int) body,
+  required Function(int, int) body,
 }) {
-  /*
-  When using 'negativeTimer', use 100 inside body(); , therfore using 'timer' set body to 0
-  */
-  body(0);
+  body(0, 100);
 
   print('Saudações do ${nome.toUpperCase()}!');
 
